24dd876089f13f27defb35bf1222fef360dfd1b4
takimi12 <tomek12olech@gmail.com>
2025-01-07 03:10:53 +0100

    correct before deploy

--- app/[lang]/admin/newsletter/page.tsx
+++ app/[lang]/admin/newsletter/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Newsletter from "../../../../components/AdminComponents/Components/Newsletter";
 import { getContacts } from "../../../../helpers/api/getEmails";
-import styles from "./Newsletter.module.scss";
 
 export default async function Newsletters() {
 	const contacts = await getContacts();

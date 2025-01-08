d96532a3042a0568e44eeef938b8883fe774c99f
takimi12 <tomek12olech@gmail.com>
2025-01-08 22:46:38 +0100

    changing page.tsx inside [category]

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wER4wuB2iA4snPgoSsXTVzdzKiRZTcH5VaB6y+xOgGw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"pMzLxhzCpvPxCONhN86PQqqlCwJx+b3Coe5Skso/Q24=\"}"
---
+++ app/[lang]/filmy-i-ebooki/[category]/page.tsx
@@ -0,0 +1,30 @@
+import Nauka from "./components/NaukaNoszenia";
+import Asymetria from "./components/Asymetriaulozeniowa";
+import Rozwoj from "./components/RozwojDziecka";
+export const generateStaticParams = async () => {
+	return [
+	  { lang: "pl", category: "Nauka-noszenia" },
+	  { lang: "pl", category: "Asymetria-ulozeniowa" },
+	  { lang: "pl", category: "Rozwoj-dziecka" },
+	];
+  };
+  
+  export default async function Page({ 
+	params 
+  }: { 
+	params: { lang: string; category: string } 
+  }) {
+	let content;
+  
+	if (params.category === "Nauka-noszenia") {
+	  content = <Nauka />;
+	} else if (params.category === "Rozwoj-dziecka") {
+	  content = <Rozwoj />;
+	} else if (params.category === "Asymetria-ulozeniowa") {
+	  content = <Asymetria />;
+	} else {
+	  content = <div>Default content or 404 page</div>;
+	}
+  
+	return <div>{content}</div>;
+  }

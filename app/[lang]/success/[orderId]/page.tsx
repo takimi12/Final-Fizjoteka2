742226f62c92c38886d913246f72d7fdcdfef350
takimi12 <tomek12olech@gmail.com>
2025-02-11 23:25:35 +0100

    correct import modules

--- app/[lang]/success/[orderId]/page.tsx
+++ app/[lang]/success/[orderId]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
-import styles from "./Page.module.scss";
+import styles from "./Success.module.scss";
 
 interface Product {
     name: string;
